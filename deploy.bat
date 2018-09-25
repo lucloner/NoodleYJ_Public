830a0d01720b66b009135206d89faaa2c687708b
Biggee <lucloner@hotmail.com>
2018-09-25 17:20:02 +0800

    更新接近公测

--- deploy.bat
+++ deploy.bat
@@ -4,6 +4,7 @@ if "%2"=="" goto :help
 if not exist platform-tools\adb.exe goto :help
 platform-tools\adb root
 platform-tools\adb remount
+if exist bootanimation.zip platform-tools\adb push bootanimation.zip /system/media/
 platform-tools\adb install %1
 platform-tools\adb push %2 /sdcard
 platform-tools\adb shell pm disable com.android.launcher3
